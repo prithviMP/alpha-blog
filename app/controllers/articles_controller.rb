f1cfa467abca3635bc876a22413a4fc46eb80485
prithviraj <prithvi_mp@yaoo.in>
2017-05-30 16:52:54 +0530

    srtyle error and message partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ end
 def update
     
 	    if @article.update(article_params)
-    			flash[:notice] = "Article was succesfully updated"
+    			flash[:success] = "Article was succesfully updated"
     			redirect_to article_path(@article)
 		else
  				render "edit"
@@ -27,7 +27,7 @@ def create
 
 		@article = Article.new(article_params)
 	    if @article.save
-		       flash[:notice] = "Article was succesfully created"
+		       flash[:success] = "Article was succesfully created"
 		       redirect_to article_path(@article)
         else
 		       render 'new'
@@ -43,7 +43,7 @@ end
 def destroy
 		
 		@article.destroy
-		flash[:notice] = "Article was succesfully deleted"
+		flash[:danger] = "Article was succesfully deleted"
 		redirect_to articles_path
 end
 

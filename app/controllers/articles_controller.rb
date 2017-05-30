63a9e26b3cd25e7741b54ea99ad8bae5019ae2d8
prithviraj <prithvi_mp@yaoo.in>
2017-05-30 14:31:55 +0530

    removing redundancy and making ready for deployment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,45 +1,47 @@
 class ArticlesController < ApplicationController
 
+before_action :set_article, only: [:edit, :update, :show, :destroy]
 def index
 		@articles = Article.all
 end	
+
 def new 
 @article = Article.new
 end
 
-		def edit
-				@article = Article.find(params[:id])
-		end	
+def edit
+		
+end	
 
-		def update
-				@article = Article.find(params[:id])
-				if @article.update(article_params)
-					flash[:notice] = "Article was succesfully updated"
-					redirect_to article_path(@article)
-				else
-					render "edit"
-				end			
-		end	
+def update
+    
+	    if @article.update(article_params)
+    			flash[:notice] = "Article was succesfully updated"
+    			redirect_to article_path(@article)
+		else
+ 				render "edit"
+	end			
+end	
+
 def create
 
-@article = Article.new(article_params)
-if @article.save
-		flash[:notice] = "Article was succesfully created"
-		redirect_to article_path(@article)
-
-else
-	render 'new'
+		@article = Article.new(article_params)
+	    if @article.save
+		       flash[:notice] = "Article was succesfully created"
+		       redirect_to article_path(@article)
+        else
+		       render 'new'
 
 		end
 	
 end	
 
 def show
-	@article = Article.find(params[:id])	
+	
 end
 
 def destroy
-		@article = Article.find(params[:id])
+		
 		@article.destroy
 		flash[:notice] = "Article was succesfully deleted"
 		redirect_to articles_path
@@ -50,6 +52,9 @@ def article_params
 		params.require(:article).permit(:title, :description)
 end
 
-
+private 
+def set_article
+		@article = Article.find(params[:id])
+end
 
 end
